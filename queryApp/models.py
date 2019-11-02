cd6221b9112d2447c9d05a79386e218b729d7907
Mahabubul Hasan <shourav6688@gmail.com>
2019-11-02 13:10:36 +0600

    data sent as context

--- queryApp/models.py
+++ queryApp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class MSE_CUSTOMERS(models.Model):
     customer_name = models.CharField("Customer Name", max_length=150, null=True, blank=True)
     customer_mobile_number = models.CharField("Mobile Number", max_length=13, null=True, blank=True)
-    customer_due_amount = models.DecimalField("Due Amount", decimal_places=4, max_digits=10, null=True, blank=True)
+    customer_due_amount = models.IntegerField("Due Amount", null=True, blank=True)
 
 
     class Meta:
--- queryApp/views.py
+++ queryApp/views.py
@@ -12,8 +12,32 @@ def data_load(request):
         obj.customer_due_amount =random.randint(10,10000)
         obj.save()
 def data_send(request):
-    customers = MSE_CUSTOMERS.objects.all()
+    # customers = MSE_CUSTOMERS.objects.all()
+    # customer_name = MSE_CUSTOMERS.objects.values_list('customer_name', flat=True)
+    # customer_mobile_number = MSE_CUSTOMERS.objects.values_list('customer_mobile_number', flat=True)
+    # customer_due_amount = MSE_CUSTOMERS.objects.values_list('customer_due_amount', flat=True)
+    # customers_dict = dict()
+    # customers_dict['customer_name'] = customer_name
+    # customers_dict['customer_mobile_number'] = customer_mobile_number
+    # customers_dict['customer_due_amount'] = customer_due_amount
+    # customers_list = list()
+    # customers_list.append(customers_dict)
+    # print(customers_list)
+    # context = {
+    #     'customer' : customers,
+    #     'customers_list' : customers_list
+    # }
+    # return render(request,'customers.html',context)
+    customer_list = list()
+    customer = MSE_CUSTOMERS.objects.all()
+    for i in customer:
+        temp_dict = dict()
+        temp_dict['customer_name'] = i.customer_name
+        temp_dict['customer_mobile_number'] = i.customer_mobile_number
+        temp_dict['customer_due_amount'] = i.customer_due_amount
+        customer_list.append(temp_dict)
     context = {
-        'customer' : customers
+        'customer' : customer_list,
     }
+    print(customer_list)
     return render(request,'customers.html',context)
